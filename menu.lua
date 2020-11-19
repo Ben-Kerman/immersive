ef60873fa7420ba1c583d413aad41faa31e353fa
Ben Kerman <ben@kermanic.org>
2020-11-20 00:59:20 +0100

    Create menu object type

---
+++ menu.lua
@@ -0,0 +1,29 @@
+Menu = {}
+Menu.__index = Menu
+
+function Menu:new(data)
+	local m = {
+		_overlay = mp.create_osd_overlay("ass-events"),
+		data = data,
+		show_bindings = false
+	}
+	setmetatable(m, Menu)
+	return m
+end
+
+function Menu:redraw()
+	local lines = {"{\\an4"}
+	for _, info in ipairs(self.data.info) do
+		table.insert(lines, string.format([[{\b1}%s{\b0}: %s]], info.name, info.value))
+	end
+
+	if self.show_bindings then
+		table.insert(lines, "{\\i1}Key Bindings{\\i0}:")
+		for _, binding in ipairs(self.data.bindings) do
+			table.insert(lines,
+				string.format([[\h\h\h\h{\b1}%s{\b0}: %s]], binding.key, binding.desc))
+		end
+	else table.insert(lines, [[{\i1}Press {\b1}h{\b0} to show key bindings{\i0}]]) end
+	self._overlay.data = table.concat(lines, "\\N")
+	self._overlay:update()
+end
