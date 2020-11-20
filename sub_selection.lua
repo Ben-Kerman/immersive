b664911525acd2fcf58c4e3f0e3f2d09ad7138a2
Ben Kerman <ben@kermanic.org>
2020-11-20 02:54:52 +0100

    Add sub select menu definition

--- sub_selection.lua
+++ sub_selection.lua
@@ -1,4 +1,26 @@
+require "menu"
 require "subtitle"
+
+local bindings = {
+	{key = "q", desc = "Set start to start of active line", action = function() end},
+	{key = "e", desc = "Set end to end of active line", action = function() end},
+	{key = "a", desc = "Select current line", action = function() end},
+	{key = "A", desc = "Toggle automatic selection", action = function() end},
+	{key = "k", desc = "Reset selection", action = function() end},
+	{key = "d", desc = "End selection and enter edit mode", action = function() end},
+	{key = "f", desc = "End selection and export immediately", action = function() end},
+	{key = "ESC", desc = "Cancel selection", action = function() end},
+}
+
+local function display_time(value)
+	if value >= 0 then return tostring(value)
+	else return "{\\i1}not set{\\i0}" end
+end
+local start = {name = "Start", value = -1, display = display_time}
+local stop = {name = "Stop", value = -1, display = display_time}
+local infos = {start, stop}
+
+local menu = Menu:new{infos = infos, bindings = bindings}
 
 local subs = {}
 
