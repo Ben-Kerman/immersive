b89597a8dfb5db570567cbde2fba4e7defd4a748
Ben Kerman <ben@kermanic.org>
2020-11-20 01:23:17 +0100

    Add and remove menu bindings

--- menu.lua
+++ menu.lua
@@ -5,7 +5,7 @@ function Menu:new(data, enabled)
 	local m = {
 		_overlay = mp.create_osd_overlay("ass-events"),
 		data = data,
-		enabled = enabled or true,
+		enabled = enabled and true,
 		show_bindings = false
 	}
 	setmetatable(m, Menu)
@@ -17,28 +17,37 @@ function Menu:enable()
 		self.show_bindings = not self.show_bindings
 		self:redraw()
 	end)
+	for i, binding in ipairs(self.data.bindings) do
+		mp.add_forced_key_binding(binding.key, "_ankisubs-menu_binding-" .. i, binding.action)
+	end
 	self.enabled = true
 	self:redraw()
 end
 
 function Menu:disable()
-	mp.remove_forced_key_binding("_ankisubs-menu_show-bindings")
+	mp.remove_key_binding("_ankisubs-menu_show-bindings")
+	for i, _ in ipairs(self.data.bindings) do
+		mp.remove_key_binding("_ankisubs-menu_binding-" .. i)
+	end
 	self.enabled = false
+	self:redraw()
 end
 
 function Menu:redraw()
-	local lines = {"{\\an4"}
-	for _, info in ipairs(self.data.info) do
-		table.insert(lines, string.format([[{\b1}%s{\b0}: %s]], info.name, info.value))
-	end
-
-	if self.show_bindings then
-		table.insert(lines, "{\\i1}Key Bindings{\\i0}:")
-		for _, binding in ipairs(self.data.bindings) do
-			table.insert(lines,
-				string.format([[\h\h\h\h{\b1}%s{\b0}: %s]], binding.key, binding.desc))
+	if self.enabled then
+		local lines = {"{\\an4"}
+		for _, info in ipairs(self.data.info) do
+			table.insert(lines, string.format([[{\b1}%s{\b0}: %s]], info.name, info.value))
 		end
-	else table.insert(lines, [[{\i1}Press {\b1}h{\b0} to show key bindings{\i0}]]) end
-	self._overlay.data = table.concat(lines, "\\N")
-	self._overlay:update()
+
+		if self.show_bindings then
+			table.insert(lines, "{\\i1}Key Bindings{\\i0}:")
+			for _, binding in ipairs(self.data.bindings) do
+				table.insert(lines,
+					string.format([[\h\h\h\h{\b1}%s{\b0}: %s]], binding.key, binding.desc))
+			end
+		else table.insert(lines, [[{\i1}Press {\b1}h{\b0} to show key bindings{\i0}]]) end
+		self._overlay.data = table.concat(lines, "\\N")
+		self._overlay:update()
+	else self._overlay:remove() end
 end
