c04008b098af53e3698eaf00569911347525cd61
Ben Kerman <ben@kermanic.org>
2020-11-20 15:08:14 +0100

    Add screenshot time

--- sub_selection.lua
+++ sub_selection.lua
@@ -3,8 +3,7 @@ require "subtitle"
 
 local menu
 local reset
-local set_start
-local set_stop
+local set_scrot, set_start, set_stop
 
 local subs = {}
 
@@ -51,6 +50,7 @@ local bindings = {
 	{key = "e", desc = "Force end to end of active line", action = function() set_stop("sub-end") end},
 	{key = "Q", desc = "Force start to current time", action = function() set_start("time-pos") end},
 	{key = "E", desc = "Force end to current time", action = function() set_stop("time-pos") end},
+	{key = "s", desc = "Take screenshot at current time", action = function() set_scrot("time-pos") end},
 	{key = "a", desc = "Select current line", action = select_sub},
 	{key = "A", desc = "Toggle automatic selection", action = toggle_auto_select},
 	{key = "k", desc = "Reset selection", action = reset},
@@ -63,9 +63,10 @@ local function display_time(value)
 	if value >= 0 then return tostring(value)
 	else return "{\\i1}not set{\\i0}" end
 end
+local scrot = {name = "Screenshot", value = -1, display = display_time}
 local start = {name = "Start", value = -1, display = display_time}
 local stop = {name = "End", value = -1, display = display_time}
-local infos = {start, stop}
+local infos = {start, stop, scrot}
 
 local function set_time(time, value)
 	local new_val
@@ -78,6 +79,7 @@ local function set_time(time, value)
 
 	menu:redraw()
 end
+set_scrot = function(value) set_time(scrot, value) end
 set_start = function(value) set_time(start, value) end
 set_stop = function(value) set_time(stop, value) end
 
