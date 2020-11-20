aa05cc7c66e94690427834294a1d87b19f324000
Ben Kerman <ben@kermanic.org>
2020-11-21 00:52:41 +0100

    Add platform detection

--- system.lua
+++ system.lua
@@ -1,5 +1,16 @@
 local system = {}
 
+system.platform = (function()
+	-- taken from mpv's built-in console
+	local default = {}
+	if mp.get_property_native("options/vo-mmcss-profile", default) ~= default then
+		return "win"
+	elseif mp.get_property_native("options/macos-force-dedicated-gpu", default) ~= default then
+		return "mac"
+	end
+	return "lnx"
+end)()
+
 function system.subprocess(args)
 	local res = mp.command_native{
 		name = "subprocess",
