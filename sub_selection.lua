d113ab13ac5d446b755fa43a88efe063b91865fc
Ben Kerman <ben@kermanic.org>
2020-11-20 03:00:16 +0100

    Reorder sub_selection.lua

--- sub_selection.lua
+++ sub_selection.lua
@@ -1,6 +1,26 @@
 require "menu"
 require "subtitle"
 
+local subs = {}
+
+local function insert_sub(sub)
+	table.insert(subs, sub)
+	table.sort(subs)
+end
+
+local function handle_sub_text(_, sub_text)
+	if sub_text ~= nil and sub_text ~= "" then
+		insert_sub(create_subtitle(sub_text, mp.get_property_number("sub-start"), mp.get_property_number("sub-end")))
+	end
+end
+
+local auto_select_active = false
+local function toggle_auto_select()
+	if auto_select_active then mp.unobserve_property(handle_sub_text)
+	else mp.observe_property("sub-text", "string", handle_sub_text) end
+	auto_select_active = not auto_select_active
+end
+
 local bindings = {
 	{key = "q", desc = "Set start to start of active line", action = function() end},
 	{key = "e", desc = "Set end to end of active line", action = function() end},
@@ -22,24 +42,5 @@ local infos = {start, stop}
 
 local menu = Menu:new{infos = infos, bindings = bindings}
 
-local subs = {}
-
-local function insert_sub(sub)
-	table.insert(subs, sub)
-	table.sort(subs)
-end
-
-local function handle_sub_text(_, sub_text)
-	if sub_text ~= nil and sub_text ~= "" then
-		insert_sub(create_subtitle(sub_text, mp.get_property_number("sub-start"), mp.get_property_number("sub-end")))
-	end
-end
-
 sub_selection = {}
 
-local auto_select_active = false
-function sub_selection.toggle_auto_select()
-	if auto_select_active then mp.unobserve_property(handle_sub_text)
-	else mp.observe_property("sub-text", "string", handle_sub_text) end
-	auto_select_active = not auto_select_active
-end
