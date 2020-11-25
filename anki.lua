4e99a02d3c5a2d36ba6dc689a37234c24d72163d
Ben Kerman <ben@kermanic.org>
2020-11-25 17:09:16 +0100

    Implement basic note field config

--- anki.lua
+++ anki.lua
@@ -4,15 +4,40 @@ local sys = require "system"
 local active_target_index = 1
 
 local anki = {
-	targets = {}
+	targets = {},
+	target_configs = {}
 }
 
+-- parse target definitions
 for name, profile, deck, note_type
 	in cfg.values.anki_targets:gmatch("%[([^:]+):([^;]+);([^;]+);([^%]]+)%]") do
 	table.insert(anki.targets, {
 		name = name, profile = profile, deck = deck, note_type = note_type
 	})
 end
+
+-- parse target config file
+local tgt_cfg_path = mp.find_config_file("script-opts/" .. mp.get_script_name() .. "-targets.conf")
+local tgt_cfg_file = io.open(tgt_cfg_path)
+local iter, lines, line = tgt_cfg_file:lines()
+while true do
+	line = iter(lines, line)
+	if line == nil then break end
+
+	local target = line:match("%[([^%]]+)%]")
+	if target then
+		anki.target_configs[target] = {}
+		repeat
+			line = iter(lines, line)
+			if not line then break end
+			local field, content = line:match("([^=]+)=(.+)")
+			if field then
+				anki.target_configs[target][field] = content
+			end
+		until line == ""
+	end
+end
+tgt_cfg_file:close()
 
 function anki.active_target()
 	return anki.targets[active_target_index]
