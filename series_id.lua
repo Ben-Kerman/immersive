31a821d4ed3e112d6b2a03cadd9047fb78d96203
Ben Kerman <ben@kermanic.org>
2020-11-25 18:56:43 +0100

    Create system for generating series IDs

---
+++ series_id.lua
@@ -0,0 +1,52 @@
+local id_conf = {}
+
+local id_cfg_path = mp.find_config_file("script-opts/" .. mp.get_script_name() .. "-ids.conf")
+local id_cfg_file = io.open(id_cfg_path, "r")
+for line in id_cfg_file:lines() do
+	local id, value = line:match("([^=]+)=(.+)")
+	if id then
+		local keywords = util.string_split(value:lower(), " ", true)
+		if keywords then
+			id_conf[id] = keywords
+		end
+	end
+end
+id_cfg_file:close()
+
+local function generate_id(filename)
+	return (filename:gsub("%.[^%.]+$", "")        -- file extension
+	                :gsub("[_%.]", " ")
+	                :gsub("%s*%[[^%]]+%]%s*", "") -- tags in []
+	                :gsub("%s*%([^%)]+%)%s*", "") -- tags in ()
+	                :lower()
+	                :gsub("%s+%-?%s*[sepv%d%.]+$", "") -- episode number at end
+	                :gsub("[^%w%s%-\128-\255]", "") -- all non-alnum ASCII chars
+	                :gsub("%s+", "-")             -- replace spaces with dash
+	                :gsub("%-+", "-")             -- duplicated dashes
+	                :gsub("^%-+", "")             -- leading dashes
+	                :gsub("%-+$", ""))            -- trailing dashes
+end
+
+local series_id = {}
+
+function series_id.get_id()
+	local filename = mp.get_property("filename"):lower()
+	local matched_id
+	for id, keywords in pairs(id_conf) do
+		local match = true
+		for _, kw in ipairs(keywords) do
+			if not filename:find(kw) then
+				match = false
+				break
+			end
+		end
+		if match then
+			matched_id = id
+			break
+		end
+	end
+	if matched_id then return matched_id end
+	return generate_id(filename)
+end
+
+return series_id
