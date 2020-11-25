b0119d03952b14b59b83325db4c3f607b802e291
Ben Kerman <ben@kermanic.org>
2020-11-25 17:10:41 +0100

    Add list filter function

--- util.lua
+++ util.lua
@@ -22,6 +22,16 @@ function util.list_max(list, cmp)
 	return max
 end
 
+function util.list_filter(list, predicate)
+	local res = {}
+	for _, val in ipairs(list) do
+		if predicate(val) then
+			table.insert(res, mapper(val))
+		end
+	end
+	return res
+end
+
 function util.list_map(list, mapper)
 	local res = {}
 	for _, val in ipairs(list) do
