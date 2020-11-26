8c5f5073dc2a8cdff9ed8cd1dc286765352d652c
Ben Kerman <ben@kermanic.org>
2020-11-26 02:29:04 +0100

    Add bit operation compatibility
    For mpv installs using LuaJIT

--- utf_8.lua
+++ utf_8.lua
@@ -1,22 +1,24 @@
+local bit_op = require "bit_compat"
+
 local function is_ascii(byte)
 	-- byte & 0b1000'0000 == 0
-	return bit32.band(byte, 0x80) == 0
+	return bit_op.band(byte, 0x80) == 0
 end
 
 local function is_lead(byte)
 	-- byte & 0b1100'0000 == 0b1100'0000
-	return bit32.band(byte, 0xc0) == 0xC0
+	return bit_op.band(byte, 0xc0) == 0xC0
 end
 
 local function is_cont(byte)
 	-- byte & 0b1100'0000 == 0b1000'0000
-	return bit32.band(byte, 0xc0) == 0x80
+	return bit_op.band(byte, 0xc0) == 0x80
 end
 
 local function char_byte_num(lead_byte)
 	local mask, num = 0x80, 0
-	while bit32.band(lead_byte, mask) ~= 0 do
-		mask, num = bit32.rshift(mask, 1), num + 1
+	while bit_op.band(lead_byte, mask) ~= 0 do
+		mask, num = bit_op.rshift(mask, 1), num + 1
 	end
 	return num
 end
@@ -33,12 +35,12 @@ local function encode(cp)
 
 	-- construct leading byte
 	local bytes = {
-		bit32.replace(bit32.rshift(cp, (byte_count - 1) * 6), 0xff, 8 - byte_count, byte_count)
+		bit_op.replace(bit_op.rshift(cp, (byte_count - 1) * 6), 0xff, 8 - byte_count, byte_count)
 	}
 
 	-- construct continuation bytes
 	for pos = byte_count - 2, 0, -1 do
-		table.insert(bytes, bit32.bor(0x80, bit32.extract(cp, pos * 6, 6)))
+		table.insert(bytes, bit_op.bor(0x80, bit_op.extract(cp, pos * 6, 6)))
 	end
 
 	return bytes
@@ -60,14 +62,14 @@ function utf_8.codepoints(str)
 		elseif is_lead(b) then
 			local byte_num = char_byte_num(b)
 			-- extract leading bits of the code point
-			next_cp = bit32.extract(b, 0, 7 - byte_num)
+			next_cp = bit_op.extract(b, 0, 7 - byte_num)
 			for k = 1, byte_num - 1 do
 				i, cb = iter(bytes, i)
 				-- no more bytes or cb isn't a cont. byte --> invalid UTF-8
 				if i == nil or not is_cont(cb) then return nil end
 
 				-- shift tmp number and or in bits from cb
-				next_cp = bit32.bor(bit32.lshift(next_cp, 6), bit32.extract(cb, 0, 6))
+				next_cp = bit_op.bor(bit_op.lshift(next_cp, 6), bit_op.extract(cb, 0, 6))
 			end
 			table.insert(cps, next_cp)
 			next_cp = 0
