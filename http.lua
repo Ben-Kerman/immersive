396c1170786a01dcb3e81329a24cb2c010fc5939
Ben Kerman <ben@kermanic.org>
2020-11-26 03:07:01 +0100

    Change curl params

--- http.lua
+++ http.lua
@@ -2,13 +2,19 @@ local sys = require "system"
 
 local http = {}
 
+local data_path = os.tmpname()
+
 function http.request(method, url, data)
+	local data_file = io.open(data_path, "w")
+	data_file:write(data)
 	local status, stdout = sys.subprocess {
 			"curl","-s",
 			url,
 			"-X", method,
-			"-d", data
+			"-H", "Content-Type: application/json; charset=UTF-8",
+			"--data-binary", "@" .. data_path
 		}
+	data_file:close()
 	if status ~= 0 then
 		mp.msg.error("HTTP " .. method .. " request for URL '" .. url .. "' failed.")
 		return nil
