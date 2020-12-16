8fbbd8b78638fb611c7dda7a5cc167e0705b9282
Ben Kerman <ben@kermanic.org>
2020-12-16 22:05:39 +0100

    Use LineTextSelect in target_select

--- target_select.lua
+++ target_select.lua
@@ -2,53 +2,19 @@ local dicts = require "dict.dicts"
 local helper = require "helper"
 require "menu"
 require "line_select"
+require "line_text_select"
 require "text_select"
 
-local line_sel, tgt_word_sel
+local tgt_word_sel
 local lookup_result, def_sel
 local sub_selection, timestamps
 local target_words = {}
 
-local function target_select_update_handler(has_sel, curs_index, segments)
-	if has_sel then
-		table.insert(segments, 2, "{\\u1}")
-		table.insert(segments, 4, "{\\u0}")
-	end
-
-	if curs_index < 0 then
-		curs_index = #segments + curs_index + 1
-	end
-	table.insert(segments, curs_index, TextSelect.default_cursor(mp.get_property_number("osd-font-size")))
-	table.insert(segments, curs_index + 1, "{\\b1}")
-	table.insert(segments, 1, "{\\b1}")
-	table.insert(segments, "{\\b0}")
-
-	local ssa_str = table.concat(segments)
-
-	if line_sel then
-		line_sel.sel_renderer = function() return ssa_str end
-		line_sel:update()
-	end
-end
-
-local function line_renderer(sub)
-	return sub:short()
-end
-
-local active_sub
-local function update_handler(sub)
-	if sub ~= active_sub then
-		active_sub = sub
-		if tgt_word_sel then tgt_word_sel:finish() end
-		tgt_word_sel = TextSelect:new(sub.text, target_select_update_handler)
-		tgt_word_sel:start()
-	end
-end
-
-local function initialize_target_select()
-	line_sel = LineSelect:new(sub_selection, nil, line_renderer, update_handler, 9)
-	update_handler(sub_selection[1])
-	line_sel:start()
+local function sel_converter(sub) return sub.text end
+local function line_renderer(sub) return sub:short() end
+local function start_tgt_sel()
+	tgt_word_sel = LineTextSelect:new(sub_selection, sel_converter, line_renderer, 9)
+	tgt_word_sel:start()
 end
 
 local function format_def(def)
@@ -64,24 +30,22 @@ end
 local function def_renderer(def)
 	return format_def(def)
 end
-
 local function sel_def_renderer(def)
 	return "{\\b1}" .. format_def(def) .. "{\\b0}"
+end
+local function start_def_sel(def)
+	def_sel = LineSelect:new(lookup_result.defs, sel_def_renderer, def_renderer, nil, 5)
+	def_sel:start()
 end
 
 local function select_target_def(prefix)
 	if def_sel then
 		local def = def_sel:finish()
-		def_sel = nil
 		table.insert(target_words, dicts[lookup_result.dict_index].get_definition(def.id))
-		initialize_target_select()
+		start_tgt_sel()
 	else
 		local selection = tgt_word_sel:finish(true)
 		if not selection then return end
-
-		line_sel:finish()
-		line_sel = nil
-		active_sub = nil
 
 		for i, dict in ipairs(dicts) do
 			local lookup_fn = prefix and dict.look_up_start or dict.look_up_exact
@@ -92,12 +56,10 @@ local function select_target_def(prefix)
 			end
 		end
 
-		if lookup_result then
-			def_sel = LineSelect:new(lookup_result.defs, sel_def_renderer, def_renderer, nil, 5)
-			def_sel:start()
+		if lookup_result then start_def_sel()
 		else
 			mp.osd_message("No entry found for selected word")
-			initialize_target_select()
+			start_tgt_sel()
 		end
 	end
 end
@@ -115,7 +77,7 @@ local target_select = {}
 function target_select.begin(sub_sel, times)
 	target_words = {}
 	sub_selection, timestamps = sub_sel, times
-	initialize_target_select()
+	start_tgt_sel()
 	menu:enable()
 end
 
