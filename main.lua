8d50f7b34ad92d5202c20d8e3287b05cd3cd2278
Ben Kerman <ben@kermanic.org>
2020-12-18 02:38:40 +0100

    Add global menu

--- main.lua
+++ main.lua
@@ -1,3 +1,34 @@
+local kbds = require "key_bindings"
 local sub_select = require "sub_select"
+local sys = require "system"
+require "menu"
 
-mp.add_key_binding("a", "ankisubs-sub-select-begin", sub_select.begin)
+-- forward declarations
+local menu
+
+local bindings = {
+	{
+		id = "begin_sub_select",
+		default = "a",
+		desc = "Start line selection",
+		action = sub_select.begin,
+		global = true
+	},
+	{
+		id = "open_global_menu",
+		default = "Ctrl+a",
+		desc = "Open the global menu",
+		action = function() menu:enable() end,
+		global = true
+	},
+	{
+		id = "close_global_menu",
+		default = "ESC",
+		desc = "Close the menu",
+		action = function() menu:disable() end
+	}
+}
+
+menu = Menu:new{bindings = bindings}
+
+kbds.add_global_bindings(bindings)
