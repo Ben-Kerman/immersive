2e71bb3362cc751be7101d3bd09b1cc60a201725
Ben Kerman <ben@kermanic.org>
2020-12-19 00:48:43 +0100

    Implement Base64 decoding

--- base64.lua
+++ base64.lua
@@ -1,16 +1,24 @@
 local bit_conv = require("bit_compat")[2]
 local util = require "util"
 
-local b64_digits = {
+local char_lookup = {
 	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
 	"Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f",
 	"g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
 	"w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/"
 }
 
+local bit_lookup = (function()
+	local res = {}
+	for i, digit in ipairs(char_lookup) do
+		res[digit:byte()] = util.list_reverse(bit_conv.to_bits(i - 1, 6))
+	end
+	return res
+end)()
+
 local function insert_digit(list, bits)
 	local pos = bit_conv.to_num(util.list_reverse(bits))
-	table.insert(list, b64_digits[pos + 1])
+	table.insert(list, char_lookup[pos + 1])
 end
 
 local base64 = {}
@@ -52,6 +60,30 @@ function base64.encode(str)
 		end
 	end
 	return table.concat(b64)
+end
+
+function base64.decode(b64)
+	local pad_start = b64:find("=")
+	local bits = {}
+	for i = 1, pad_start and pad_start - 1 or #b64 do
+		for _, bit in ipairs(bit_lookup[b64:byte(i)]) do
+			table.insert(bits, bit)
+		end
+	end
+
+	local str = {}
+
+	local from = 1
+	while from <= #bits do
+		local to = from + 7
+		if to > #bits then break end
+
+		local byte = bit_conv.to_num(util.list_reverse(util.list_range(bits, from, to)))
+		table.insert(str, string.char(byte))
+
+		from = from + 8
+	end
+	return table.concat(str)
 end
 
 return base64
