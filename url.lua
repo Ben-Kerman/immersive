1a9a273e7a30db7b3eae1ef1120adfab2168abae
Ben Kerman <ben@kermanic.org>
2020-12-19 17:24:54 +0100

    Write function for URL encoding data

---
+++ url.lua
@@ -0,0 +1,27 @@
+local util = require "util"
+
+local unreserved = util.list_map({
+	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+	"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+	"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+	"-", "_", ".", "~"
+}, function(char) return char:byte() end)
+
+local url = {}
+
+function url.encode(str)
+	local encoded = {}
+	for i = 1, #str do
+		local byte = str:byte(i)
+		local code
+		if util.list_find(unreserved, byte) then
+			code = string.char(byte)
+		else code = string.format("%%%02X", byte) end
+		table.insert(encoded, code)
+	end
+	return table.concat(encoded)
+end
+
+return url
