072707f0e23ed9f21643ead7dbce2e7874451856
Ben Kerman <ben@kermanic.org>
2020-12-23 02:10:46 +0100

    Change message duration based on level

--- message.lua
+++ message.lua
@@ -3,7 +3,16 @@ local util = require "util"
 local overlay = mp.create_osd_overlay("ass-events")
 overlay.z = 127
 
-local levels = {"fatal", "error", "warn", "info", "verbose", "debug", "trace"}
+local osd_duration = mp.get_property_number("osd-duration") / 1000
+local levels = {
+	fatal = 0,
+	error = math.max(osd_duration, 10),
+	warn = math.max(osd_duration, 5),
+	info = osd_duration,
+	verbose = osd_duration,
+	debug = osd_duration,
+	trace = osd_duration
+}
 
 local messages = {}
 
@@ -33,7 +42,7 @@ local function add_msg(level, text, timeout)
 	}
 	table.insert(messages, msg)
 
-	if not timeout then timeout = 10 end
+	if not timeout then timeout = levels[level] end
 	if timeout ~= 0 then
 		mp.add_timeout(timeout, function()
 			local _, pos = util.list_find(messages, msg)
@@ -50,7 +59,7 @@ end
 
 local message = {}
 
-for _, level in ipairs(levels) do
+for level, _ in pairs(levels) do
 	message[level] = function(text, timeout)
 		add_msg(level, text, timeout)
 	end
