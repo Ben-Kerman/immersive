4138e37fa83a1067bfd5ebe9b5400dbd34586c5d
Ben Kerman <ben@kermanic.org>
2020-12-24 01:32:51 +0100

    Add title to series identification

--- series_id.lua
+++ series_id.lua
@@ -6,22 +6,27 @@ local config = util.map_map(cfg.load_subcfg("series"), function(_, series)
 		return nil
 	end
 	return series.name, {
+		title = series.entries.title,
 		keywords = util.string_split(series.entries.keywords:lower(), " ", true)
 	}
 end)
 
-local function generate_id(filename)
+local function generate_title(filename)
 	return (filename:gsub("%.[^%.]+$", "")        -- file extension
 	                :gsub("[_%.]", " ")
 	                :gsub("%s*%[[^%]]+%]%s*", "") -- tags in []
 	                :gsub("%s*%([^%)]+%)%s*", "") -- tags in ()
-	                :lower()
-	                :gsub("%s+%-?%s*[sepv%d%.]+$", "") -- episode number at end
-	                :gsub("[^%w%s%-\128-\255]", "") -- all non-alnum ASCII chars
-	                :gsub("%s+", "-")             -- replace spaces with dash
-	                :gsub("%-+", "-")             -- duplicated dashes
-	                :gsub("^%-+", "")             -- leading dashes
-	                :gsub("%-+$", ""))            -- trailing dashes
+	                :gsub("%s+%-?%s*[SsEePpVv%d%.]+$", "")) -- episode number at end
+end
+
+local function generate_id(filename)
+	local title = generate_title(filename)
+	return (title:lower()
+	             :gsub("[^%w%s%-\128-\255]", "")  -- all non-alnum ASCII chars
+	             :gsub("%s+", "-")                -- replace spaces with dash
+	             :gsub("%-+", "-")                -- duplicated dashes
+	             :gsub("^%-+", "")                -- leading dashes
+	             :gsub("%-+$", ""))               -- trailing dashes
 end
 
 local function match_filename(filename)
@@ -51,6 +56,16 @@ function series_id.id()
 	local id = match_filename(filename_lc)
 	if id then return id, true
 	else return generate_id(filename_lc), false end
+end
+
+function series_id.title()
+	local filename = mp.get_property("filename")
+	if not filename then return nil end
+
+	local id, values = match_filename(filename)
+	if id and values.title then
+		return values.title, true
+	else return generate_title(filename), false end
 end
 
 return series_id
