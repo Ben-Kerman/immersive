0c088c3231f529b424ab9e95fba84a03a786beb6
Ben Kerman <ben@kermanic.org>
2021-04-23 04:36:54 +0200

    Add standalone Japanese deinflection search transform

---
+++ dict/transform/deinflect-japanese.lua
@@ -0,0 +1,152 @@
+-- Immersive is licensed under the terms of the GNU GPL v3: https://www.gnu.org/licenses/; © 2021 Ben Kerman
+
+local ext = require "utility.extension"
+local utf_8 = require "utility.utf_8"
+
+local special_cases = {
+	-- する
+	["為"] = "する",
+	["さ"] = "する",
+	["し"] = "する",
+	["せ"] = "する",
+	["為よ"] = "する",
+	["せよ"] = "する",
+	["為ろ"] = "する",
+	["しろ"] = "する",
+	-- 来る
+	["来"] = "来る",
+	["こ"] = "来る",
+	["き"] = "来る",
+	["来い"] = "来る",
+	-- 行く
+	["いっ"] = "行く",
+	["行っ"] = "行く",
+	-- イ音便
+	["いらっしゃい"] = "いらっしゃる",
+	["おっしゃい"] = "おっしゃる",
+	["仰っしゃい"] = "おっしゃる",
+	["仰しゃい"] = "おっしゃる",
+	["仰い"] = "おっしゃる",
+	["仰有い"] = "おっしゃる",
+	["ください"] = "くださる",
+	["下ださい"] = "くださる",
+	["下さい"] = "くださる",
+	["ござい"] = "ござる",
+	["御座い"] = "ござる",
+	["ご座い"] = "ござる",
+	["なさい"] = "なさる",
+	["為さい"] = "為さる"
+}
+
+local ichidan = {
+	"い", "き", "ぎ", "し", "じ", "ち", "ぢ", "に", "ひ", "び", "ぴ", "み", "り",
+	"え", "け", "げ", "せ", "ぜ", "て", "で", "ね", "へ", "べ", "ぺ", "め", "れ"
+}
+
+local basic_godan = {
+	-- 未然形
+	["わ"] = "う",
+	["か"] = "く",
+	["が"] = "ぐ",
+	["さ"] = "す",
+	["た"] = "つ",
+	["な"] = "ぬ",
+	["ば"] = "ぶ",
+	["ま"] = "む",
+	["ら"] = "る",
+	-- 未然形（音便）
+	["お"] = "う",
+	["こ"] = "く",
+	["ご"] = "ぐ",
+	["そ"] = "す",
+	["と"] = "つ",
+	["の"] = "ぬ",
+	["ぼ"] = "ぶ",
+	["も"] = "む",
+	["ろ"] = "る",
+	-- 連用形
+	["い"] = "う",
+	["き"] = "く",
+	["ぎ"] = "ぐ",
+	["し"] = "す",
+	["ち"] = "つ",
+	["に"] = "ぬ",
+	["び"] = "ぶ",
+	["み"] = "む",
+	["り"] = "る",
+	-- 仮定形・命令形
+	["え"] = "う",
+	["け"] = "く",
+	["げ"] = "ぐ",
+	["せ"] = "す",
+	["て"] = "つ",
+	["ね"] = "ぬ",
+	["べ"] = "ぶ",
+	["め"] = "む",
+	["れ"] = "る"
+}
+
+local onbin_godan = {
+	["い"] = {"く", "ぐ"},
+	["っ"] = {"う", "つ", "る"},
+	["ん"] = {"ぬ", "ぶ", "む"}
+}
+
+local keiyoushi = {
+	"かっ", "かれ", "かろ", "く", "けれ", "さ"
+}
+
+local function is_hiragana(cp)
+	return 0x3041 <= cp and cp <= 0x3096
+end
+
+local function deinflect(tgt, word, infl, base)
+	local function insert(sub_end, suf)
+		table.insert(tgt, word:sub(1, sub_end) .. suf)
+	end
+
+	local first_pos, last_pos = word:find(infl .. "$")
+	if first_pos and first_pos > 1 then
+		local sub_end = first_pos - 1
+		if type(base) == "table" then
+			for _, suf in ipairs(base) do
+				insert(sub_end, suf)
+			end
+		else insert(sub_end, base) end
+	end
+end
+
+return function()
+	return function(word)
+		local base_forms = {}
+
+		local cdpts = utf_8.codepoints(word)
+		if not is_hiragana(cdpts[#cdpts]) then
+			table.insert(base_forms, word .. "る")
+		end
+		for _, stem in ipairs(ichidan) do
+			if word:find(stem .. "$") then
+				table.insert(base_forms, word .. "る")
+			end
+		end
+
+		for infl, base in pairs(basic_godan) do
+			deinflect(base_forms, word, infl, base)
+		end
+		for infl, bases in pairs(onbin_godan) do
+			deinflect(base_forms, word, infl, bases)
+		end
+
+		for _, infl in ipairs(keiyoushi) do
+			deinflect(base_forms, word, infl, "い")
+		end
+
+		for infl, base in pairs(special_cases) do
+			if word == infl then
+				table.insert(base_forms, base)
+			end
+		end
+
+		return base_forms
+	end
+end
